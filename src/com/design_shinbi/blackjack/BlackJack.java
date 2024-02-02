74fc67e1c5d77dbec2c4249f855cfad23ec54e92
Chika Arakawa <ahirugon.222@gmail.com>
2024-02-02 11:33:39 +0900

    最初のコミット

---
+++ src/com/design_shinbi/blackjack/BlackJack.java
@@ -0,0 +1,51 @@
+package com.design_shinbi.blackjack;
+
+import java.util.Scanner;
+
+public class BlackJack {
+	public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        start(scanner);
+        scanner.close();
+    }
+
+
+    public static void start(Scanner scanner) {
+    	Player player = new Player(scanner);
+    	Dealer dealer = new Dealer();
+    	Stock stock = new Stock();
+    	
+    	player.start(stock); //山札二枚引いた
+    	dealer.start(stock); //山札二枚引いた
+    	
+    	dealer.display();
+    	
+    	player.play(stock); //playerがplay
+    	dealer.play(stock); //dealerがplay
+    	
+    	showResult(player, dealer);
+    
+
+
+    }
+    
+    public static void showResult(Attender player, Attender dealer) {
+        dealer.display();
+        player.display();
+        
+        int dealerStrength = dealer.calculateStrength();
+        int playerStrength = player.calculateStrength();
+        
+
+        
+        if(dealerStrength < playerStrength) {
+			System.out.print("あなたの勝ち!");
+		}
+		else if(dealerStrength > playerStrength) {
+			System.out.println("あなたの負け");
+		}
+		else {
+			System.out.println("引き分けです");
+		}
+    }
+}
